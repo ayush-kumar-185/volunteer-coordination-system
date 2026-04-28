e0b6ca53a667112a7bb0a0c161f0420e9e3e2471
Ayush Kumar <kr.ayush185@gmail.com>
2026-04-28 16:27:45 +0530

    Minor changes

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, error: 'Internal Server Error' })
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
